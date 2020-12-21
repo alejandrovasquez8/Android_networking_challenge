a4838bfc96b0359ffb67dfab4041b02b42ec8214
alejandrovasquez <avasquez@osfactory.co>
2020-12-21 16:32:18 -0500

    test finished

---
+++ Application/src/main/java/com/example/android/networkconnect/Response.java
@@ -0,0 +1,55 @@
+package com.example.android.networkconnect;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Response {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("species")
+    @Expose
+    private String species;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
